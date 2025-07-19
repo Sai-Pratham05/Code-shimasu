1525cfb2f1d9de6017fdc24525174882631a7c89
Sai Pratham <saipratham007@gmail.com>
2025-07-19 23:04:40 +0530

    Edit footer: update content and improve layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import { Blocks } from "lucide-react";
 import Link from "next/link";
 
 function Footer() {
@@ -8,11 +7,11 @@ function Footer() {
       <div className="max-w-7xl mx-auto px-4 py-8">
         <div className="flex flex-col md:flex-row items-center justify-between gap-4">
           <div className="flex items-center gap-2 text-gray-400">
-            {/* <Blocks className="size-5" /> */}
+  
             <span>© 2025 All Rights Reserved.</span>
           </div>
           <div className="flex justify-center items-center gap-2 text-gray-400 mb-4 md:mb-0">
-          {/* <Blocks className="size-5" /> */}
+
           <span>Made with ❤️ By Sai Pratham | Code-shimasu</span>
         </div>
           <div className="flex items-center gap-6">
